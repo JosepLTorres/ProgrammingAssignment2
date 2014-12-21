6c269c5431be2283a5a38d0416f6cd3f15fc3d20
Josep L. Torres <josep.l.torres@gmail.com>
2014-12-21 22:35:48 +0100

    First version of assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL ##set m default value to NULL (if cacheSolve has not previously been used)
+  set<-function(y){ ##set the value of the matrix
+    x<<-y ## caches the inputted matrix
+    m<<-NULL ##set m default value to NULL
+  }
+  get<-function() x
+  setmatriu<-function(solve) m<<- solve
+  getmatriu<-function() m
+  ## Create a list for the functions used
+  list(set=set, get=get,
+       setmatriu=setmatriu,
+       getmatriu=getmatriu)
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m<-x$getmatriu() ## get inverse matrix if it was previously calculated
+  if(!is.null(m)){ ## check if cacheSolve has been run before
+    message("getting cached data")
+    return(m)
+  }
+  matriu<-x$get() ##load input matrix
+  m<-solve(matriu, ...) ##calculate inverse matrix
+  x$setmatriu(m) ##cache the calculated inverse matrix
+  m ##return calculated inverse matrix
 }
